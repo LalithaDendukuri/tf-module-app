c46e224a1bfe3a16d7a36e24180937a2f4b5585c
Lalitha <lalitha.d339@gmail.com>
2023-09-13 05:35:48 +0530

    terraform variables

--- main.tf
+++ main.tf
@@ -82,6 +82,23 @@ resource "aws_lb_target_group" "main" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 }
+resource "aws_lb_listener_rule" "main" {
+  listener_arn = var.private_listener
+  priority     = var.lb_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
+  condition {
+    host_header  {
+      //values = ["${var.component}-${var.env}.dljrobo.online"]
+      values = [ var.component == "front-end" ? "${var.env}.dljrobo.online"  :"${var.component}-${var.env}.dljrobo.online"]
+    }
+  }
+
+}
 
 resource "aws_lb_target_group" "public" {
   count  = var.component == "front-end" ? 1 : 0
@@ -100,23 +117,6 @@ resource "aws_lb_target_group_attachment" "public" {
   port              = 80
   availability_zone = "all"
 }
-resource "aws_lb_listener_rule" "main" {
-  listener_arn = var.private_listener
-  priority     = var.lb_priority
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
-  }
-
-  condition {
-    host_header  {
-      //values = ["${var.component}-${var.env}.dljrobo.online"]
-      values = [ var.component == "front-end" ? "${var.env}.dljrobo.online"  :"${var.component}-${var.env}.dljrobo.online"]
-    }
-  }
-
-}
 resource "aws_lb_listener_rule" "public" {
   count = var.component == "front-end" ? 1 : 0
   listener_arn = var.public_listener
@@ -130,7 +130,7 @@ resource "aws_lb_listener_rule" "public" {
   condition {
     host_header {
       //values = ["${var.component}-${var.env}.dljrobo.online"]
-      values = [ var.component == "front-end" ? "${var.env}.dljrobo.online"  :"${var.component}-${var.env}.dljrobo.online"]
+      values = [ "${var.env}.dljrobo.online" ]
     }
   }
 
