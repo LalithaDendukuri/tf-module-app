1a4280bf71111119420f8121abbdefb61c8f8a14
Lalitha <lalitha.d339@gmail.com>
2023-09-17 11:00:56 +0530

    terraform variables

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
   name_prefix      = "${var.env}-${var.component}"
   tags             = merge(var.tags, { tf-module-name = "app" }, { env = var.env })
-  parameters        =concat(var.parameters, [var.component])
+  parameters        = concat(var.parameters, [var.component])
 //  policy_resources ="arn:aws:ssm:us-east-1:896540652966:parameter/docdb.dev.*"
-  policy_resources = [ for i in local.parameters : "arn:aws:ssm:us-east-1:896540652966:parameter/${var.component}.${i}.${var.env}.*"]
+  policy_resources = [ for i in local.parameters : "arn:aws:ssm:us-east-1:896540652966:parameter/${i}.${var.env}.*"]
 
 }
